45e2945ef5c9f6bf01578240f329f018694f65e8
Filipe Abalada <filipe.abalada@gmail.com>
2020-05-03 13:10:41 -0400

    fixed pom

--- coding-exercise/src/main/java/ltse/coding_exercise/App.java
+++ coding-exercise/src/main/java/ltse/coding_exercise/App.java
@@ -1,6 +1,9 @@
 package ltse.coding_exercise;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import ltse.coding_exercise.extractor.Parser;
 
@@ -12,10 +15,20 @@ public class App
 {
     public static void main( String[] args )
     {
+	if (args.length != 3) {
+	    System.err.println("Usage : java ltse.coding_exercise.extractor.App");
+	    return;
+	}
         Parser parser = new Parser();
         try {
-	    parser.extract("resources/firms.txt", "resources/symbols.txt", "resources/trades.csv");
+            parser.extract(args[0], args[1], args[2]);
+	    //parser.extract("resources/firms.txt", "resources/symbols.txt", "resources/trades.csv");
 	    
+            if (Files.notExists(Paths.get("out"))) {
+        	File outputDirectory = new File("out");
+        	outputDirectory.mkdir();
+            }
+            
 	    parser.writeAcceptedIds("out/acceptedIds.csv");
 	    parser.writeRejectedIds("out/rejectedIds.csv");
 	    parser.writeAcceptedOrders("out/acceptedOrders.txt");
